a4f47dd91b263c25e844cdd306eb83b05b3544cb
Tachibana Sho <JP21988@w022511902019m-7875.local>
2020-04-07 13:26:17 +0900

    first commit

--- hello/intro_code.sh
+++ hello/intro_code.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # clone starter code
 git clone https://github.com/line/line-liff-v2-starter.git
-cd line-liff-v2-starter/
 # download heroku cli
 curl https://cli-assets.heroku.com/install.sh | sh
 echo "サンプルコードのダウンロードとHeroku CLIのインストールが完了しました"
