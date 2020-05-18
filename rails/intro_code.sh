430f74849056031e3680b57269cf7b4e3c9f3628
Tachibana Sho <JP21988@w022511902019m-7875.local>
2020-05-18 19:12:08 +0900

    Added ruby

--- rails/intro_code.sh
+++ rails/intro_code.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Installing rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
@@ -7,9 +6,7 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 source ~/.bash_profile
 apt-get install -y zlib1g-dev
-# Installing rbenv
 rbenv install 2.7.1
 rbenv global 2.7.1
-# Installing ngrok
 npm i -g ngrok
 ruby -v
